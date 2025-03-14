7b0827ff404de05e0e38567d3878df256b3eaaa0
nortveeb2408 <conv.19aivfiiyfe2h7@fleep.io>
2025-03-14 18:08:48 +0200

    loeng 07 - 1

--- public/admin.js
+++ public/admin.js
@@ -1,4 +1,5 @@
 const adminSisu = document.getElementById('admin-sisu')
+let matkad = []
 
 async function loeSonumid() {
     const result = await fetch('/api/sonumid', )
@@ -11,20 +12,17 @@ async function loeSonumid() {
     console.log(sonumid)
 }
 
-async function loeMatkad() {
+async function loeMatkadJaKuvaLeht() {
     const result = await fetch('/api/matkad', )
     if (!result.ok) {
         console.log('Viga andmete lugemisel')
         return;
     }
 
-    const matkad = await result.json()
-    console.log(matkad)
+    matkad = await result.json()
     adminSisu.innerHTML = looLeheHTML(matkad)
+    naitaParemPaan(0)
 }
-
-loeSonumid()
-loeMatkad()
 
 function looLeheHTML(matkad) {
     const vasakPaan = looVasakPaanHTML(matkad)
@@ -33,7 +31,7 @@ function looLeheHTML(matkad) {
         <div class="col-4">
             ${vasakPaan}
         </div>
-        <div class="col-8">
+        <div id="parem-paan-sisu" class="col-8">
             siia tuleb parem paan
         </div>
      </div>
@@ -43,12 +41,37 @@ function looLeheHTML(matkad) {
 function looVasakPaanHTML(matkad) {
     console.log(matkad)
     let vasakPaan = ''
+    let id = 0
     for (matk of matkad) {
         vasakPaan += `
-        <div>
+        <div class="vasak-paan-valik" onclick="naitaParemPaan(${id})">
             ${matk.nimetus}
         </div>
         `
+        id += 1
     }
     return vasakPaan
 }
+
+function naitaParemPaan(matkaId) {
+    const paremPaan = document.getElementById('parem-paan-sisu')
+    const matk = matkad[matkaId]
+
+    let osalejadHTML = ''
+    matk.osalejad.forEach(email => {
+        osalejadHTML += `
+        <li>${email}  </li>
+        `
+    });
+
+    const paremPaanHtml = `
+    <h3>${matk.nimetus}</h3>
+    <div>${matk.kirjeldus}</div>
+    <ol>
+        ${osalejadHTML}
+    </ol
+    `
+    paremPaan.innerHTML = paremPaanHtml
+}
+
+loeMatkadJaKuvaLeht()
